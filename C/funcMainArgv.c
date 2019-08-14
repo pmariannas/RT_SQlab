8ba024ec6784be404f22eb893ba5af1c7fe3af28
Marianna <pmariannas@gmail.com>
2019-08-14 13:16:55 +0300

    Add files via upload

---
+++ C/funcMainArgv.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+    char* arg1 = argv[1] ;
+    char* arg2 = argv[2];
+    int valueReturn = 0;
+    int i, count1=0, count2=0;
+    int len1 = strlen(arg1);  
+    int len2 = strlen(arg2);  
+
+    if(argc == 3)     
+    {
+        valueReturn = isNumber(arg1);
+        if(isNumber(arg1)==0 && isString(arg2)==0 )
+        {
+            printf("age:%d name:%s\n",  atoi(arg1), arg2);
+        }
+        else if(isNumber(arg2)==0 && isString(arg1)==0 )
+        {
+            printf("age:%d name:%s\n",  atoi(arg2), arg1);
+        }
+        else 
+        {
+            printf("error");
+        
+           
+        }
+        isString(arg1);
+       /*
+       for(i=0 ; i<len1; i++)
+       {
+           if(isdigit(arg1[i]))
+           {
+               count1++;
+           }
+       }
+       for(i=0 ; i<len2; i++)
+       {
+           if(isalpha(arg2[i]))
+           {
+               count2++;
+           }
+       }
+
+       if(count1==len1&&count2==len2)
+       {
+           printf("age:%d name:%s\n",  atoi(arg1), arg2);
+
+       }
+           
+       */
+        
+    }
+
+}
+
+int isNumber(char* arg1)
+{
+    int len1 = strlen(arg1);  
+    int count=0, i;
+    for(i=0 ; i<len1; i++)
+       {
+           if(isdigit(arg1[i]))
+           {
+               count++;
+           }
+       }
+    if(count==len1)
+    {
+        return 0;
+
+    }
+    else
+    {
+        return -1;
+    }
+}
+int isString(char* arg1)
+{
+    int len1 = strlen(arg1);  
+    int count=0, i;
+    for(i=0 ; i<len1; i++)
+       {
+           if(isalpha(arg1[i]))
+           {
+               count++;
+           }
+       }
+    if(count==len1)
+    {
+        return 0;
+
+    }
+    else
+    {
+        return -1;
+    }
+}
