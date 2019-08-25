25f55ccc569f09a46fad5725e545b3e32ff0eda5
Marianna <pmariannas@gmail.com>
2019-08-25 23:20:45 +0300

    Update BST.c

--- Data structure/binary_tree/BST.c
+++ Data structure/binary_tree/BST.c
@@ -1,32 +1,42 @@
-#include <stdlib.h>
-#include <time.h>
 #include "BST.h"
+#include <stdlib.h>
 
-typedef struct Node{
-	int _data;
-	struct Node* _left;
-    struct Node* _right;
+typedef struct Node Node;
+
+struct Node{
+	int _value;
+	Node* _left;
+    Node* _right;
 };
 
 struct Tree{
     int _numOfNodes;
-	struct Node* _root;
+	Node* _root;
 };
 
+static Node* createNode(int value);
+static Node* findMinimum(Node* current);
+static Node* myDeleteNode(Node* current, int value);
+
+
+/*-----------------------------------------------*/
 Tree* createTree()
 {
-    Tree* t1 ;
-    t1 = malloc(sizeof(Tree));
+    Tree* t1;
+
+    t1 = (Tree*)malloc(sizeof(Tree));
     
     if(t1==NULL)
     {
         return NULL;
     }
 
+    t1->_root = NULL;
+
 	return t1;
 }
-
-Node* createNode(int value)
+/*-----------------------------------------------*/
+static Node* createNode(int value)
 {
     Node* node ;
     node = malloc(sizeof(Node));
@@ -36,40 +46,176 @@ Node* createNode(int value)
         return NULL;
     }
 
-    node->_data = value;
+    node->_value = value;
     node->_left = NULL;
     node->_right = NULL;
 
 	return node;
 }
-
-
-int insertChild(Tree* t, int value)
+/*-----------------------------------------------*/
+int insertNode(Tree* tree, int value)
 {
-    Node* child;
+    Node* node;
+    Node* current;
+    Node* parent;
 
-    /*check if root exist with value*/
-    if(t->_root == NULL)
+    if(tree == NULL)
     {
-        t->_root->_data = value;
-        t->_root->_left = NULL;
-        t->_root->_right = NULL;
-        return t;
+        return 1;
+    }   
+    
+    node = createNode(value);
+
+    if(node == NULL)
+    {
+        return -1;
     }
-
-    child = createNode(value);
-
-    if(t->_root->_data > child->_data)
+    /*check if tree is empty*/
+    if(tree->_root == NULL)
     {
-        t->_root->_left = child;
-
+        tree->_root = node;
+        return 0;
+    }
+    /*if tree is not empty*/
+    current = tree->_root;
+    parent = NULL;
+    /*move to place to put the value*/
+    while(current != NULL)
+    {
+        parent = current;
+        if(current->_value > value)
+        {
+            current = current->_left;
+        }
+        else
+        {
+            current = current->_right;
+        }
+    }
+    /*put the node in place*/
+    if(parent->_value > value)
+    {
+        parent->_left = node;
     }
     else
     {
-        t->_root->_right = child;
+        parent->_right = node;
     }
+
+    return 0;
+}
+/*-----------------------------------------------*/
+int searchBTree(Tree* tree, int value)
+{
+    Node* current;
+    Node* parent;
+
+    if(tree == NULL)
+    {
+        return -1;
+    }   
+
+    /*check if tree is empty*/
+    if(tree->_root == NULL)
+    {
+        return -1;
+    }
+    /*if tree is not empty*/
+    current = tree->_root;
+    parent = NULL;
     
-   	
+    while(current != NULL)
+    {
+        if(current->_value == value)
+        {
+            return 1;
+        }
+
+        parent = current;
+        if(current->_value > value)
+        {
+            current = current->_left;
+        }
+        else
+        {
+            current = current->_right;
+        }
+    }
+    return 0;
+}
+/*-----------------------------------------------*/
+int deleteNode(Tree* tree, int value)
+{
+    Node* current;
+    Node* result;
+    if(tree == NULL)
+    {
+        return -1;
+    }   
+    /*check if tree is empty*/
+    if(tree->_root == NULL)
+    {
+        return -1;
+    }
+    /*if tree is not empty*/
+    current = tree->_root;
+    result = myDeleteNode(current, value);
+    if(result == NULL)
+    {
+        return 0;
+    }
+    else
+        return 1;
+}
+/*-----------------------------------------------*/
+static Node* myDeleteNode(Node* current, int value)
+{
+    if(current==NULL)
+    {
+        return NULL;
+    }
+    else if(current->_value > value)
+    {
+        current->_left = myDeleteNode(current->_left, value);
+    }
+    else if(current->_value < value)
+    {
+        current->_right = myDeleteNode(current->_right, value);
+    }
+    else if(current->_value == value)/*node found - delete*/
+    {
+        /*node has no children*/
+        if(current->_left == NULL && current->_right == NULL)
+        {
+            current = NULL;
+        }
+        else if(current->_left == NULL)
+        {
+            current = current->_right;
+        }
+        else if(current->_right == NULL)
+        {
+            current = current->_left;
+        }
+        else /*has 2 children*/
+        {
+            Node* temp = findMinimum(current->_right);
+            current->_value = temp->_value;
+            current->_right = myDeleteNode(current->_right, temp->_value);
+        }
+    }
+    else /*node not found (not exist in tree)*/
+    {
+        return NULL;
+    }
+    return current;
 }
 
-
+static Node* findMinimum(Node* current)
+{
+    if(current->_left == NULL)
+    {   
+        return current;
+    }
+    return findMinimum(current->_left);
+}
