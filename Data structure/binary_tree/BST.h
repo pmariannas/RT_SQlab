7bf969cec24f939566f4696077414d5ebb99ba5d
Marianna <pmariannas@gmail.com>
2019-08-25 23:22:28 +0300

    Update BST.h

--- Data structure/binary_tree/BST.h
+++ Data structure/binary_tree/BST.h
@@ -1,20 +1,13 @@
 #ifndef H_BST
 #define H_BST
-#include <stdlib.h>
-
-typedef enum  {OK, AllocationError, WrongInput} BSTStatus;
 
 typedef struct Tree Tree;
-/*
-typedef void (* elementDestroy)(void *_elem, void *context);
-typedef int (* elementCompare)(void *_elemA, void *_elemB);
 
-*/
 
 Tree* createTree();
-int insertChild(Tree* root, int child);
-BSTStatus searchBTree();
-BSTStatus deleteChild();
-BSTStatus walkTree();
+int insertNode(Tree* tree, int value);
+int searchBTree(Tree* tree, int value);
+int deleteNode(Tree* tree, int value);
+
 
 #endif
