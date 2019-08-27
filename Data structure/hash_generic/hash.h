0f9aead58c508d0d9ccf01c4a8ff3be954651104
Marianna <pmariannas@gmail.com>
2019-08-27 15:21:28 +0300

    Update hash.h

--- Data structure/hash_generic/hash.h
+++ Data structure/hash_generic/hash.h
@@ -2,19 +2,21 @@
 #define  H_HASH
 
 
-typedef enum  {OK, AllocationError,WrongInput, NullPointer} AdtStatus;
+typedef enum  {OK, AllocationError,WrongInput, NullPointer, NotFound} AdtStatus;
 
-typedef struct hashTable hashTable;
-typedef struct Node Node;
+typedef struct HashTable HashTable;
 
 typedef unsigned long (*hashFunc)(void* key);
 typedef int (*compareElementsFunc)(void* key1, void* key2);
 
 
-AdtStatus createHashTable(hashTable** hashT, size_t size, hashFunc hashF, compareElementsFunc compF);
-AdtStatus insertHashTable(hashTable* hashT, void* key, void* value);
-AdtStatus findHashTable(hashTable* hashT, void* key, void* value); /*???*/
-AdtStatus desroyHashTable(hashTable* table); /*???*/
+AdtStatus createHashTable(HashTable** hashT, size_t size, hashFunc hashF, compareElementsFunc compF);
+AdtStatus insertHashTable(HashTable* hashT, void* key, void* value);
+/*
+AdtStatus findHashTable(HashTable* hashT, void* key, void* value); 
+AdtStatus desroyHashTable(HashTable* table); 
+*/
+
 /*
 AdtStatus forEachHashTable(hashTable* hashT);
 */
