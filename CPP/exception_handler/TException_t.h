889ee199a7d0f732256f20d0c7e6e9c107548ae2
Marianna <pmariannas@gmail.com>
2019-10-06 09:18:08 +0300

    Add files via upload

--- CPP/exception_handler/TException_t.h
+++ CPP/exception_handler/TException_t.h
@@ -7,14 +7,14 @@ template <class TEH> class TException_t
 {
     public:
         ~TException_t(){} //CTOR
-        TException_t(TEH exception, string message,  int line, string name); //CTOR
+        TException_t(const TEH& exception, const string& message,  const int& line, const string& name); //CTOR
         TException_t(const TException_t& obj){} //copy CTOR
         TException_t& operator=(const TException_t& obj){} //operator =
 
-        const TEH& getException()const;/* { return m_exception;}*/
-        int getLineOfException()const; /*{ return m_lineNum;}*/
-        string getMessage()const; /*{ return m_mess;}*/
-        string getFileName()const; /*{ return fName;}*/
+        const TEH& getException()const { return m_exception;}
+        const int& getLineOfException()const{ return m_lineNum;}
+        const string& getMessage()const{ return m_mess;}
+        const string& getFileName()const{ return m_fName;}
         
 
     private:
@@ -30,7 +30,7 @@ template <class TEH> class TException_t
 // }
 
 template <class TEH>
-TException_t<TEH>::TException_t(TEH exception, string message, int line, string name)
+TException_t<TEH>::TException_t(const TEH& exception, const string& message,  const int& line, const string& name)
 {
     m_exception = exception;
     m_lineNum = line;
@@ -39,7 +39,7 @@ TException_t<TEH>::TException_t(TEH exception, string message, int line, string 
 }
 
 
-
+/*
 template <class TEH>
 const TEH& TException_t<TEH>::getException()const
 {
@@ -63,7 +63,7 @@ string TException_t<TEH>::getFileName()const
 { 
     return m_fName;
 }
-
+*/
 
 
 #endif
