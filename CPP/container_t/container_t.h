307444b32e84e8b3c1b6a1371e0fea47f702c284
Marianna <pmariannas@gmail.com>
2019-10-09 22:34:04 +0300

    Add files via upload

--- CPP/container_t/container_t.h
+++ CPP/container_t/container_t.h
@@ -1,30 +1,84 @@
 #ifndef CONTAINER_T_CLASS_H
 #define CONTAINER_T_CLASS_H
 
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <list>
+#include <typeinfo>
+#include <algorithm>
+#include <string>
+
 using namespace std;
 
 template<class T, class C> class tContainer_t
 {
     public:
-        ~tContainer_t(); //DTOR
-        tContainer_t(); //CTOR
+        virtual ~tContainer_t(){} //DTOR
+        tContainer_t(){} //CTOR
        
-        bool isEmpty(); //return true/false according to is container empty or not
-        size_t getNumOfElements(); // return number of element in container
-        void insertEnd(const tContainer_t& cont); // insert a new element in the end of container
-        const T& getFirstElement()const; //return first element in container
-        const T& getLastElement()const; //return last element in container
-        bool findElement(tContainer_t& cont); 
-        void removeElement();
-        void removeAllElements();
-        void removeAndDeleteElement();
-        void removeAndDeleteAllElements();
-        //tContainer_t<T, C> operator []();
+        bool isEmpty()const {return m_container.empty();} //return true/false according to is container empty or not
+        size_t getNumOfElements()const {return m_container.size();} // return number of element in container
+        void insertEnd(T* value) { m_container.push_back(value);} // insert a new element in the end of container
+        T* getFirstElement()const 
+        {return isEmpty()? throw string("container empty"):(m_container.front());} //return first element in container
+        T* getLastElement()const 
+        {return isEmpty()?throw string("container empty"):(m_container.back());} //return last element in container
+        /*
+        bool findElement(tContainer_t& cont)//<algorithm> --> find_if - find element int range 
+        {
+            m_it.find_if(m_container.begin(), m_container.end(), )
+            
+        } 
+        */
+        //void removeElement();
+        void removeAllElements()
+        {
+            if(isEmpty())
+            {
+                throw string("container empty");
+            }
+            else
+            {
+                iter m_it = m_container.begin();
+                while(m_it != m_container.end())
+                {
+                    m_it = m_container.erase(m_it);
+                }
+            }
+        }
+        //void removeAndDeleteElement();
+        void removeAndDeleteAllElements()
+        {
+            if(isEmpty())
+            {
+                throw string("container empty");
+            }
+            else
+            {
+                iter m_it = m_container.begin();
+                T* tmp;
+                while(m_it != m_container.end())
+                {
+                    tmp = *m_it;
+                    m_it = m_container.erase(m_it);
+                    delete tmp;
+                } 
+            }  
+        }
+        /*T* operator [](size_t index)const
+        {
+           
+        }
+        */
 
 
     private:
-        typedef typename tContainer_t::
-        C container;
+        typedef typename C::iterator iter;
+        typedef typename C::const_iterator c_iter;
+
+        C m_container;
+
         tContainer_t(const tContainer_t& cont); //copy CTOR
         tContainer_t<T, C> operator=(const tContainer_t& cont); //operator =
 
---
+++ CPP/container_t/mainTest.cpp
@@ -0,0 +1,156 @@
+#include "container_t.h"
+
+
+
+int main()
+{
+    unsigned int i = 1;
+    unsigned int op=1;
+    tContainer_t <int, vector<int*> > intArray;
+
+    cout<< "choose vector(2) || list(3) || default(0)-vector"<<endl;
+    cin>>op;
+    while (op)
+    {
+        if(op==2)
+        {
+            tContainer_t <int, vector<int*> > intArray;
+            break;
+        }
+        else if(op==3)
+        {
+            tContainer_t <int, list<int*> > intArray; 
+            break;  
+        }
+        else
+        {
+            cout<< "choose vector(2) || list(3)"<<endl;
+            cin >> op;
+        }
+
+    }
+    
+    while (i)
+    {
+        cout << "\nChoose option: \n"<< endl;
+        cout << "0 - exit " << endl;
+        cout << "1 - insert element container " << endl;
+        cout << "2 - get Number Of Elements" << endl;
+        cout << "3 - get First Element in container" << endl;
+        cout << "4 - get Last Element in container" << endl;
+        cout << "5 - remove All Elements" << endl;
+        cout << "6 - remove and delete All Elements" << endl;
+        cout << "7 - " << endl;
+        cout << "8 - " << endl;
+        cout << "9 - " << endl;
+        cout << "10 - " << endl;
+        
+
+        cin >> i;
+        switch (i)
+        {
+            case 0:
+                i = 0;
+                break;
+            case 1:
+            {
+                size_t num;
+                int* x = new int;
+
+                cout<<"enter element: ";
+                cin>>num;
+                *x = num;
+                intArray.insertEnd(x);
+            
+
+                break;
+            }
+            case 2:
+            {
+                
+                cout<< "number of element in container: " << intArray.getNumOfElements() <<endl;
+                break;
+            }
+            case 3:
+            {
+                try
+                {
+                    cout<< "First Element in container: " <<intArray.getFirstElement() <<endl;
+
+                }
+                catch(string e)
+                {
+                    cout<< e << endl;
+                }
+                
+                break;
+            }
+            case 4:
+            {
+                try
+                {
+                    cout<< "Last Element in container: " <<intArray.getLastElement() <<endl;
+                }
+                catch(string e)
+                {
+                    cout<< e << endl;
+                }
+                
+
+                break;
+            }
+            case 5:
+            {
+                try
+                {
+                    intArray.removeAllElements();
+                    cout<< "All Elements removed!"<< endl;
+                }
+                catch(string e)
+                {
+                    cout<< e << endl;
+                }
+                
+                break;
+            }
+            case 6:
+            {
+                try
+                {
+                    intArray.removeAndDeleteAllElements();
+                    cout<<"All Elements removed And Deleted" <<endl;
+                }
+                catch(string e)
+                {
+                    cout<< e << endl;
+                }
+                break;
+            }
+            case 7:
+            {
+
+                break;
+            }
+            case 8:
+            {
+            
+                break;
+            }
+            case 9:
+            {
+                
+                break;
+            }
+            case 10:
+            {
+            
+                break;
+            }
+        
+        }
+    }
+
+
+    return 0;
+}
+
